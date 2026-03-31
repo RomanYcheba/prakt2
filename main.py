2f1a91246a849500e26ea5b552b7affabc2ce167
root <root@localhost.localdomain>
2026-03-31 19:00:29 +0300

    first commit

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ def add(a, b):
     return a + b
 if __name__ == "__main__":
     print(f"2 + 2 = {add(2, 2)}")
-
+def subtract(a, b):   # <-- Новая функция
+    return a - b
+if __name__ == "__main__":
+    print(f"2 + 2 = {add(2, 2)}")
+    print(f"10 - 3 = {subtract(10, 3)}") # <-- Новая строка
